d5a619d96009f2bb01c2b5c332de06fcea615c39
Alex Jercan <jercan_alex27@yahoo.com>
2023-04-04 12:31:17 +0300

    feat: added motion detection for optimization

--- client.py
+++ client.py
@@ -6,37 +6,50 @@ import cv2
 from websockets import connect
 
 
+async def check_frame(websocket, frame):
+    data = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)[..., ::-1]
+    height, width, _ = data.shape
+    data = struct.pack("<I", height) + struct.pack("<I", width) + data.tobytes()
+    await websocket.send(data)
+    response = await websocket.recv()
+    response = json.loads(response)
+
+    face_locations = response["face_locations"]
+    face_names = response["face_names"]
+
+    for (top, right, bottom, left), name in zip(face_locations, face_names):
+        top *= 4
+        right *= 4
+        bottom *= 4
+        left *= 4
+
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+
+        cv2.rectangle(
+            frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED
+        )
+        font = cv2.FONT_HERSHEY_DUPLEX
+        cv2.putText(
+            frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1
+        )
+
+
 async def webcam(websocket):
     video_capture = cv2.VideoCapture(0)
+
+    fgbg = cv2.createBackgroundSubtractorMOG2()
 
     while True:
         _, frame = video_capture.read()
 
-        data = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)[..., ::-1]
-        height, width, _ = data.shape
-        data = struct.pack("<I", height) + struct.pack("<I", width) + data.tobytes()
-        await websocket.send(data)
-        response = await websocket.recv()
-        response = json.loads(response)
+        fgmask = fgbg.apply(frame)
 
-        face_locations = response["face_locations"]
-        face_names = response["face_names"]
+        _, thresh = cv2.threshold(fgmask, 100, 255, cv2.THRESH_BINARY)
 
-        for (top, right, bottom, left), name in zip(face_locations, face_names):
-            top *= 4
-            right *= 4
-            bottom *= 4
-            left *= 4
+        contours, _ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
-
-            cv2.rectangle(
-                frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED
-            )
-            font = cv2.FONT_HERSHEY_DUPLEX
-            cv2.putText(
-                frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1
-            )
+        if any(cv2.contourArea(cnt) > 1000 for cnt in contours):
+            await check_frame(websocket, frame)
 
         cv2.imshow("Video", frame)
 
